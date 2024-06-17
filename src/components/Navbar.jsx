788a36340f1cbfadf266be2614cff3d5ee36e36a
vixxxaz <99724809+vixxxaz@users.noreply.github.com>
2024-06-17 16:08:52 +0200

    again seo commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <header className="bg-slate-400 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a href="#about" className="title-font font-medium text-white mb-4 md:mb-0">
+        <a href="#about" className="title-font font-medium text-black mb-4 md:mb-0">
           <Link to="/about" className="ml-3 text-xl">
             Gardet Vincent
           </Link>
@@ -25,7 +25,7 @@ export default function Navbar() {
         <Link
           target='iframe_a'
           to="/contact"
-          className="inline-flex items-center bg-gray-600 font-bold border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded hover:text-white  text-base mt-4 md:mt-0">
+          className="inline-flex items-center bg-gray-300 font-bold border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded hover:text-white  text-base mt-4 md:mt-0">
           Contactez moi
           <FaArrowRight className="w-4 h-4 ml-1" />
         </Link>
